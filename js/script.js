ec90e3658ac06054316a470edaf1d821112873d6
0zzycodes <horlalekan123@gmail.com>
2019-12-26 09:31:23 +0100

    added search functionality

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ let temDB = []
 
 const todayD = new Date()
 const proxy = 'https://cors-anywhere.herokuapp.com/';
+const api = '701c955448e09bde13555965f059341b';
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(position => {
@@ -20,9 +21,20 @@ if (navigator.geolocation) {
     long = position.coords.longitude;
 
     console.log(position);
-    const api = '701c955448e09bde13555965f059341b';
     const url = `${proxy}https://api.darksky.net/forecast/${api}/${lat},${long}`;
+    fetchData(url)
 
+    fetch(`${proxy}https://geocode.xyz/${lat},${long}?json=1 `)
+      .then(res => res.json())
+      .then(res => {
+        console.log(res); {
+          /* <img src="assets/sun.svg" class="anime"></img> */
+        }
+        city.innerHTML = ` ${res.city}`;
+      });
+  })
+
+  function fetchData(url) {
     fetch(url)
       .then(response => response.json())
       .then(data => {
@@ -39,7 +51,7 @@ if (navigator.geolocation) {
         } = data.currently
         desc.innerHTML = summary
         hum.innerHTML = `${humidity * 100}%`
-        speed.innerHTML = `${windSpeed.toFixed(0)} km/h`
+        speed.innerHTML = `${windSpeed.toFixed(0)} mph`
         pres.innerHTML = `${pressure.toFixed(0)} mb`
         oz.innerHTML = ozone.toFixed(0)
 
@@ -49,6 +61,7 @@ if (navigator.geolocation) {
         date.innerHTML = `${tm.dayName} ${tm.date}  ${tm.month}`
         temp.innerHTML = `<h3>${ tem.toFixed(0) }°C</h3> <span><strong>Feels Like: ${aptem.toFixed(0)}°C </strong></span>`
         let holder = data.daily.data
+        output.innerHTML = null
         holder.shift()
         holder.forEach(dayForcast => {
           const {
@@ -68,99 +81,7 @@ if (navigator.geolocation) {
 
           let weatherIcon
           Ico = weatherIcon
-          if (icon === "clear-day") {
-            document.querySelector('.current').style.background = '#1b1b1ba6'
-            bgImage.style.background = "url('./assets/afternoon.gif')"
-            bgImage.style.backgroundPosition = "center"
-            bgImage.style.backgroundSize = "cover"
-            app.style.background = "url('./assets/afternoon.gif')"
-            app.style.backgroundPosition = "center"
-            app.style.backgroundSize = "cover"
-            weatherIcon = `<img src="assets/sun.svg"></img>`;
-          } else if (icon === "clear-night") {
-            document.querySelector('.current').style.background = '#1b1b1ba6'
-            bgImage.style.background = "url('./assets/clear-night.gif')"
-            bgImage.style.backgroundPosition = "center"
-            bgImage.style.backgroundSize = "cover"
-            app.style.background = "url('./assets/clear-night.gif')"
-            app.style.backgroundPosition = "center"
-            app.style.backgroundSize = "cover"
-            weatherIcon = `<i class="wi wi-night-clear"></i>`;
-          } else if (icon === "rain") {
-            document.querySelector('.current').style.background = '#1b1b1ba6'
-            bgImage.style.background = "url('./assets/raining.gif')"
-            bgImage.style.backgroundPosition = "center"
-            bgImage.style.backgroundSize = "cover"
-            app.style.background = "url('./assets/raining.gif')"
-            app.style.backgroundPosition = "center"
-            app.style.backgroundSize = "cover"
-            weatherIcon = `<i class="wi wi-rain"></i>`;
-          } else if (icon === "snow") {
-            document.querySelector('.current').style.background = '#1b1b1ba6'
-            bgImage.style.background = "url('./assets/snow.gif')"
-            bgImage.style.backgroundPosition = "center"
-            bgImage.style.backgroundSize = "cover"
-            app.style.background = "url('./assets/snow.gif')"
-            app.style.backgroundPosition = "center"
-            app.style.backgroundSize = "cover"
-            weatherIcon = `<i class="wi wi-snow"></i>`;
-          } else if (icon === "sleet") {
-            document.querySelector('.current').style.background = '#1b1b1ba6'
-            bgImage.style.background = "url('./assets/sleet.gif')"
-            bgImage.style.backgroundPosition = "center"
-            bgImage.style.backgroundSize = "cover"
-            app.style.background = "url('./assets/sleet.gif')"
-            app.style.backgroundPosition = "center"
-            app.style.backgroundSize = "cover"
-            weatherIcon = `<i class="wi wi-sleet"></i>`;
-          } else if (icon === "wind") {
-            document.querySelector('.current').style.background = '#1b1b1ba6'
-            bgImage.style.background = "url('./assets/wind.gif')"
-            bgImage.style.backgroundPosition = "center"
-            bgImage.style.backgroundSize = "cover"
-            app.style.background = "url('./assets/wind.gif')"
-            app.style.backgroundPosition = "center"
-            app.style.backgroundSize = "cover"
-            weatherIcon = `<i class="wi wi-windy"></i>`;
-          } else if (icon === "fog") {
-            document.querySelector('.current').style.background = '#1b1b1ba6'
-            bgImage.style.background = "url('./assets/foggy.gif')"
-            bgImage.style.backgroundPosition = "center"
-            bgImage.style.backgroundSize = "cover"
-            app.style.background = "url('./assets/foggy.gif')"
-            app.style.backgroundPosition = "center"
-            app.style.backgroundSize = "cover"
-            weatherIcon = `<i class="wi wi-fog"></i>`;
-          } else if (icon === "cloudy") {
-            document.querySelector('.current').style.background = '#1b1b1ba6'
-            bgImage.style.background = "url('./assets/cloudy.gif')"
-            bgImage.style.backgroundPosition = "center"
-            bgImage.style.backgroundSize = "cover"
-            app.style.background = "url('./assets/cloudy.gif')"
-            app.style.backgroundPosition = "center"
-            app.style.backgroundSize = "cover"
-            weatherIcon = `<i class="wi wi-cloudy"></i>`;
-          } else if (icon === "partly-cloudy-day") {
-            document.querySelector('.current').style.background = '#1b1b1ba6'
-            bgImage.style.background = "url('./assets/partly-cloudy-day.gif')"
-            bgImage.style.backgroundPosition = "center"
-            bgImage.style.backgroundSize = "cover"
-            app.style.background = "url('./assets/partly-cloudy-day.gif')"
-            app.style.backgroundPosition = "center"
-            app.style.backgroundSize = "cover"
-            weatherIcon = `<img src="assets/partlyCloudy.svg"></img>`;
-          } else if (icon === "partly-cloudy-night") {
-            document.querySelector('.current').style.background = '#1b1b1ba6'
-            bgImage.style.background = "url('./assets/partly-cloudy-night.gif')"
-            bgImage.style.backgroundPosition = "center"
-            bgImage.style.backgroundSize = "cover"
-            app.style.background = "url('./assets/partly-cloudy-night.gif')"
-            app.style.backgroundPosition = "center"
-            app.style.backgroundSize = "cover"
-            weatherIcon = `<i class="wi wi-night-alt-cloudy"></i>`;
-          } else {
-            weatherIcon = `<i class="wi wi-day-sunny"></i>`;
-          }
+          changeBG(icon)
           const convertedTime = timeConverter(time),
             convertedMaxTemp = (5 / 9) * (temperatureMax - 32),
             convertedMinTemp = (5 / 9) * (temperatureMin - 32)
@@ -170,20 +91,40 @@ if (navigator.geolocation) {
           }
           temDB.push(forecaste)
           localStorage.setItem("Forcasts", JSON.stringify(temDB))
+
           output.innerHTML += `
-          <li onClick=showDetails("${convertedTime.dayName}")><span>${convertedTime.dayName}</span> <span>${convertedMinTemp.toFixed(0)}°C - ${convertedMaxTemp.toFixed(0)}°C</span></li>
-          `
+                            <li onClick=showDetails("${convertedTime.dayName}")><span>${convertedTime.dayName}</span> <span>${convertedMinTemp.toFixed(0)}°C - ${convertedMaxTemp.toFixed(0)}°C</span></li>
+                            `
         })
       })
-    fetch(`${proxy}https://geocode.xyz/${lat},${long}?json=1 `)
+  }
+
+  document.querySelector('form').addEventListener('submit',
+    e => {
+      e.preventDefault()
+      let value = document.querySelector('input').value
+      console.log(value);
+      getFavData(value.toLowerCase())
+
+    })
+
+  function getFavData(id) {
+    fetch(`${proxy}https://geocode.xyz/${id}?json=1 `)
       .then(res => res.json())
       .then(res => {
-        console.log(res); {
-          /* <img src="assets/sun.svg" class="anime"></img> */
-        }
-        city.innerHTML = ` ${res.city}`;
+        const {
+          latt,
+          longt
+        } = res
+        let url = `${proxy}https://api.darksky.net/forecast/${api}/${latt},${longt}`;
+        console.log(res);
+        fetchData(url)
+        city.innerHTML = ` ${res.standard.city}`;
       });
-  })
+  }
+
+
+
 }
 // CONVERT UNIX TIME STAMP
 
@@ -205,6 +146,102 @@ function timeConverter(UNIX_timestamp) {
   };
 }
 
+function changeBG(icon) {
+  if (icon === "clear-day") {
+    document.querySelector('.current').style.background = '#1b1b1ba6'
+    bgImage.style.background = "url('./assets/afternoon.gif')"
+    bgImage.style.backgroundPosition = "center"
+    bgImage.style.backgroundSize = "cover"
+    app.style.background = "url('./assets/afternoon.gif')"
+    app.style.backgroundPosition = "center"
+    app.style.backgroundSize = "cover"
+    weatherIcon = `<img src="assets/sun.svg"></img>`;
+  } else if (icon === "clear-night") {
+    document.querySelector('.current').style.background = '#1b1b1ba6'
+    bgImage.style.background = "url('./assets/clear-night.gif')"
+    bgImage.style.backgroundPosition = "center"
+    bgImage.style.backgroundSize = "cover"
+    app.style.background = "url('./assets/clear-night.gif')"
+    app.style.backgroundPosition = "center"
+    app.style.backgroundSize = "cover"
+    weatherIcon = `<i class="wi wi-night-clear"></i>`;
+  } else if (icon === "rain") {
+    document.querySelector('.current').style.background = '#1b1b1ba6'
+    bgImage.style.background = "url('./assets/raining.gif')"
+    bgImage.style.backgroundPosition = "center"
+    bgImage.style.backgroundSize = "cover"
+    app.style.background = "url('./assets/raining.gif')"
+    app.style.backgroundPosition = "center"
+    app.style.backgroundSize = "cover"
+    weatherIcon = `<i class="wi wi-rain"></i>`;
+  } else if (icon === "snow") {
+    document.querySelector('.current').style.background = '#1b1b1ba6'
+    bgImage.style.background = "url('./assets/snow.gif')"
+    bgImage.style.backgroundPosition = "center"
+    bgImage.style.backgroundSize = "cover"
+    app.style.background = "url('./assets/snow.gif')"
+    app.style.backgroundPosition = "center"
+    app.style.backgroundSize = "cover"
+    weatherIcon = `<i class="wi wi-snow"></i>`;
+  } else if (icon === "sleet") {
+    document.querySelector('.current').style.background = '#1b1b1ba6'
+    bgImage.style.background = "url('./assets/sleet.gif')"
+    bgImage.style.backgroundPosition = "center"
+    bgImage.style.backgroundSize = "cover"
+    app.style.background = "url('./assets/sleet.gif')"
+    app.style.backgroundPosition = "center"
+    app.style.backgroundSize = "cover"
+    weatherIcon = `<i class="wi wi-sleet"></i>`;
+  } else if (icon === "wind") {
+    document.querySelector('.current').style.background = '#1b1b1ba6'
+    bgImage.style.background = "url('./assets/wind.gif')"
+    bgImage.style.backgroundPosition = "center"
+    bgImage.style.backgroundSize = "cover"
+    app.style.background = "url('./assets/wind.gif')"
+    app.style.backgroundPosition = "center"
+    app.style.backgroundSize = "cover"
+    weatherIcon = `<i class="wi wi-windy"></i>`;
+  } else if (icon === "fog") {
+    document.querySelector('.current').style.background = '#1b1b1ba6'
+    bgImage.style.background = "url('./assets/foggy.gif')"
+    bgImage.style.backgroundPosition = "center"
+    bgImage.style.backgroundSize = "cover"
+    app.style.background = "url('./assets/foggy.gif')"
+    app.style.backgroundPosition = "center"
+    app.style.backgroundSize = "cover"
+    weatherIcon = `<i class="wi wi-fog"></i>`;
+  } else if (icon === "cloudy") {
+    document.querySelector('.current').style.background = '#1b1b1ba6'
+    bgImage.style.background = "url('./assets/cloudy.gif')"
+    bgImage.style.backgroundPosition = "center"
+    bgImage.style.backgroundSize = "cover"
+    app.style.background = "url('./assets/cloudy.gif')"
+    app.style.backgroundPosition = "center"
+    app.style.backgroundSize = "cover"
+    weatherIcon = `<i class="wi wi-cloudy"></i>`;
+  } else if (icon === "partly-cloudy-day") {
+    document.querySelector('.current').style.background = '#1b1b1ba6'
+    bgImage.style.background = "url('./assets/partly-cloudy-day.gif')"
+    bgImage.style.backgroundPosition = "center"
+    bgImage.style.backgroundSize = "cover"
+    app.style.background = "url('./assets/partly-cloudy-day.gif')"
+    app.style.backgroundPosition = "center"
+    app.style.backgroundSize = "cover"
+    weatherIcon = `<img src="assets/partlyCloudy.svg"></img>`;
+  } else if (icon === "partly-cloudy-night") {
+    document.querySelector('.current').style.background = '#1b1b1ba6'
+    bgImage.style.background = "url('./assets/partly-cloudy-night.gif')"
+    bgImage.style.backgroundPosition = "center"
+    bgImage.style.backgroundSize = "cover"
+    app.style.background = "url('./assets/partly-cloudy-night.gif')"
+    app.style.backgroundPosition = "center"
+    app.style.backgroundSize = "cover"
+    weatherIcon = `<i class="wi wi-night-alt-cloudy"></i>`;
+  } else {
+    weatherIcon = `<i class="wi wi-day-sunny"></i>`;
+  }
+}
+
 function showDetails(id) {
   const data = JSON.parse(localStorage.getItem("Forcasts"))
   let result = data.filter(item => item.convertedTime.dayName === id);
@@ -222,18 +259,9 @@ function showDetails(id) {
       time,
       windSpeed
     } = item.dayForcast
-    console.log(icon,
-      humidity,
-      ozone,
-      pressure,
-      summary,
-      sunriseTime,
-      sunsetTime,
-      temperatureMax,
-      temperatureMin,
-      time,
-      windSpeed);
     desc.innerHTML = summary
+    humidity.toFixed(0)
+    changeBG(icon)
     hum.innerHTML = `${humidity * 100}%`
     speed.innerHTML = `${windSpeed.toFixed(0)} km/h`
     pres.innerHTML = `${pressure.toFixed(0)} mb`
